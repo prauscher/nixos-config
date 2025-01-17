cf569d7d1871a631313b2009bbd0f7cf6269052f
Patrick Rauscher <prauscher@prauscher.de>
2025-01-17 18:04:42 +0100

    cleanup old versions automatically

--- configuration.nix
+++ configuration.nix
@@ -12,8 +12,15 @@
     ];
 
   # Bootloader.
-  boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.systemd-boot.configurationLimit = 10;
+  nix.gc = {
+    automatic = true;
+    dates = "weekly";
+    options = "--delete-older-than 1w";
+  };
+  nix.settings.auto-optimise-store = true;
 
   networking.hostName = "ramona";
 
