510deb51a34a15c4fe5b3e06b1bd2fb0fb2cdcf1
Patrick Rauscher <prauscher@prauscher.de>
2026-01-04 21:55:36 +0100

    fix mymenu shortcut for chat

--- nixos/utils/mymenu/mymenu.sh
+++ nixos/utils/mymenu/mymenu.sh
@@ -17,7 +17,7 @@ case "$OPTION" in
 run) exec sh -c "rofi -show drun" ;;
 keepass) exec sh -c "rofi-keepass /home/prauscher/Nextcloud/Passwords.kdbx" ;;
 thwkeepass) exec sh -c "rofi-keepass /home/prauscher/THW/Nextcloud/3.\ Gruppen/OV-Führung/Passwörter.kdbx" ;;
-chat) exec sh -c "alacritty -e sh -c 'TERM=xterm256color ssh -t shells.darmstadt.ccc.de \"tmux attach\"'" ;;
+chat) exec sh -c "alacritty -e sh -c 'ssh -t shells.darmstadt.ccc.de \"tmux attach\"'" ;;
 lock) exec sh -c "lock-screen" ;;
 suspend) exec sh -c "systemctl suspend" ;;
 esac
