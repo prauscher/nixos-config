50e6d07cb69e2409c303308027ad89291baa9eef
Patrick Rauscher <prauscher@prauscher.de>
2026-09-03 00:38:42 +0200

    bump to nixos-26.05

--- flake.nix
+++ flake.nix
@@ -2,10 +2,10 @@
   description = "A very basic flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-25.11";
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-26.05";
 
     home-manager = {
-      url = "github:nix-community/home-manager?ref=release-25.11";
+      url = "github:nix-community/home-manager?ref=release-26.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
